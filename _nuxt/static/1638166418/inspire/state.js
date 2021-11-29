5b13ced95e2b518116b532d1026e1fea3a154f62
range3 <range3@users.noreply.github.com>
2021-11-29 11:12:23 +0000

    deploy: eec70775bdef0ae3373f641e47b81100b9c5d11e

--- _nuxt/static/1638166418/inspire/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmnist-nuxt\u002F_nuxt\u002Fstatic\u002F1638166418",layout:"default",error:a,serverRendered:true,routePath:"\u002Finspire",config:{_app:{basePath:"\u002Fmnist-nuxt\u002F",assetsPath:"\u002Fmnist-nuxt\u002F_nuxt\u002F",cdnURL:a}}}}(null));
